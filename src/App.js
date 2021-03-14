47a74a549f88bf955e0e9ab5a9424e8cd29df272
thorgriffs <22.kelliking@gmail.com>
2021-03-14 17:48:23 -0400

    edited title and removed comments on index.html, added component About.js and imported it to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/About'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <Route exact path='/' component={About} />
           
         </header>
       </div>
