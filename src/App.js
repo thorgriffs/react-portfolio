e64024ad5b45aed1e9dc4f52fe97626813ee34e3
thorgriffs <22.kelliking@gmail.com>
2021-03-14 23:19:21 -0400

    added fontawesome and created the Contact.js component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/About'
+// import Navbar from '.components/Navbar'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <Route exact path='/' component={About} />
-          
+          {/* <Navbar /> */}
+          <Switch>
+            <Route exact path='/' component={About} />
+            <Route exact path='/contact' component={Contact} />
+          </Switch>
         </header>
       </div>
     </Router>
